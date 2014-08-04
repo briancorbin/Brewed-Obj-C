18e03c8c0deb12632b5c188206ace716af7093b3
Brian Corbin <brian.william.corbin@gmail.com>
2014-08-04 14:37:55 -0400

    Initial Commit

---
+++ Brewed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Brewed
+//
+//  Created by Brian Corbin on 8/4/14.
+//  Copyright (c) 2014 Caramel Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Brewed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Brewed
+//
+//  Created by Brian Corbin on 8/4/14.
+//  Copyright (c) 2014 Caramel Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
